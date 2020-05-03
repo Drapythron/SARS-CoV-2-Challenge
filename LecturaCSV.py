2f81f280043b78c95d1ec6e54d2b88bff5c5844c
Sergio Beltrán Guerrer <45850795+sergiobeltranguerrero@users.noreply.github.com>
2020-05-03 16:40:23 +0200

    Error en unificación de países

--- LecturaCSV.py
+++ LecturaCSV.py
@@ -3,38 +3,42 @@ Tuppla: (accession, length, geoLocation)"""
 
 import csv
 
+
 sequences = []
 geoLocations = []
 analysisList = []
 
+
 def mergeSort(data):
-    if len(data) <= 2:  # Cuando tenemos un pack individual o una pareja
+    if len(data) <= 2:
         if len(data) == 2:
-            if data[0] > data[1]:  # Si es pareja y no estan en orden, hacemos un swap
-                data[0], data[1] = data[1], data[0]
+            if data[0] > data[1]:  # swap(data[0], data[1])
+                temp = data[0]
+                data[0] = data[1]
+                data[1] = temp
         return data
 
     result = []
-    m = len(data) // 2  # Función recursiva del mètodo Divide&Conquere
+    m = len(data) // 2
     dataL = mergeSort(data[:m])
     dataR = mergeSort(data[m:])
 
     i = j = 0
 
-    while i < len(dataL) and j < len(dataR):    # Vamos añadiendo a result los datos de forma ordenada
+    while i < len(dataL) and j < len(dataR):
         if dataL[i] < dataR[j]:
             result.append(dataL[i])
             i += 1
         else:
             result.append(dataR[j])
             j += 1
-    # Añadimos los valores que quedasen a un lado si el otro ya ha llegado a su fin
+
     result += dataL[i:]
     result += dataR[j:]
     return result
 
 
-def median(list):   # Función màtematica de la mediana
+def median(list):
     list = mergeSort(list)
     n = len(list)
     if n % 2 == 1:
@@ -45,34 +49,32 @@ def median(list):   # Función màtematica de la mediana
 
 
 def mediana(geoLocation):
-    idems = []      # Desará los length
+    lengths = []
     for tupla in sequences:
-        if tupla[2] == geoLocation:     # Comprovamos que estos al país que toca
+        if tupla[2] == geoLocation:
             analysisList.append(tupla)
-            idems.append(int(tupla[1])) # idems tendrá los datos de solo los length
-    med = median(idems)  # Mediana realizada con funciones propias
-
-    # Ahora devolveremos el valor length correspondiente a la posición de la mediana
-    if idems.count(median):     # NO CAPITXI EL MEDIAN ESTE
-        indexMedian = idems.index(med)
+            lengths.append(int(tupla[1]))
+    med = median(lengths)  # Mediana realizada con funciones propias
+    # med = statistics.median(idems) Calcular la mediana mediante la librería statistics.
+    if lengths.count(median):
+        indexMedian = lengths.index(med)
         result = analysisList[indexMedian]
-    else:  # Para cuando la mediana no coincida con ningun elemento de la lista
-        result = analysisList[min(range(len(idems)), key=lambda i: abs(idems[i] - med))]
-
+    else:  # Para cuando la mediana no cioncide con ningun elemento de la lista
+        result = analysisList[min(range(len(lengths)), key=lambda i: abs(lengths[i] - med))]
     analysisList.clear()
     return result
 
 
 def saveGeoLocation(reader):
     locations = []
-    row1 = next(reader)  # Eliminamos la linea de el encabezado
+    row1 = next(reader)
     for loc in row1:
-        locations.append([loc])  # Vamos saparando los diferentes paises
+        locations.append([loc])
     for row in reader:
         pos = 0
         for loc in row:
             if loc != '':
-                locations[pos].append(loc)  # Dentro de cada pais, añadimos todas sus diferentes ubicaciones
+                locations[pos].append(loc)
             pos += 1
     return locations
 
@@ -80,28 +82,26 @@ def saveGeoLocation(reader):
 def newLocation(oldLocation, locations):
     for country in locations:
         if country.count(oldLocation):
-            return country[0]  # Devuelve el país, sin la región si existiese
+            return country[0]
+    return oldLocation
+
 
 
 if __name__ == '__main__':
     with open('sequences.csv', newline='') as File:
-        with open('geoLocations.csv', newline='') as LocationsFile:
-            # Abrimos fichero con los datos de las regiones y paises
-            reader = csv.reader(LocationsFile, delimiter=';')
-            locations = saveGeoLocation(reader)
+        with open('geoLocations.csv', newline='') as FileLoc:
+            reader = csv.reader(FileLoc, delimiter=';')
+            locs = saveGeoLocation(reader)
             reader = csv.reader(File)
-            row1 = next(reader)  # Eliminamos la linea del encabezado
+            row1 = next(reader)  # Eliminamos la linea de el encabezado
             for row in reader:
                 accession = row[0]
                 length = row[5]
-                geoLocation = newLocation(row[12], locations)
-                # Cogemos la ubicación del sequences (ej: USA: CA) y la traduciremos en solo el país (USA)
-
+                geoLocation = newLocation(row[12], locs)
                 if accession != '' and length != '' and geoLocation != '':  # Para evitar que no introdizcamos algun dato vacio
-                    sequences.append((accession, length, geoLocation))  # Añadimos la tupla con los datos
-                    if geoLocations.count(geoLocation) == 0:  # POSAR COMENTARI
+                    sequences.append((accession, length, geoLocation))
+                    if geoLocations.count(geoLocation) == 0:
                         geoLocations.append(geoLocation)
-
-            for geoLocation in geoLocations:  # Calculamos las medianas en cada país
+            for geoLocation in geoLocations:
                 res = mediana(geoLocation)
                 print(res)
