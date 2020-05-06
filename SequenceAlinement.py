b6cd2781932953213d8be50d5a4f21d69b8819b0
Martí Serratosa Sedó <64153663+martiserrase@users.noreply.github.com>
2020-05-06 07:44:09 +0200

    Comentaris

--- SequenceAlinement.py
+++ SequenceAlinement.py
@@ -3,20 +3,22 @@ sequences = []
 
 if __name__ == '__main__':
 
-    #PROCESO DE LECTURA DEL FICHERO
+    # PROCESO DE LECTURA DEL FICHERO
     seqFASTA = open('sequencesFASTA.fasta')
     reader = seqFASTA.readline()
     pos = -1
-    while reader != '':
-        if reader[0] == '>':
+    while reader != '':     # Hasta que no haya ni una letra
+        if reader[0] == '>':    # Si nos encontramos con '>' es el inicio de los datos de un virus
             pos += 1
             reader = reader.replace('>', '').replace('\n', '').replace(' ', '')
-            line = reader.split("|")
+            line = reader.split("|")    # Separamos el accession del geoLocation
             sequences.append(line)
             sequences[pos].append('')
-        else:
+        else:   # Estamos a las lineas del RNA
             reader = reader.replace('\n', '')
             sequences[pos][2] += reader
 
         reader = seqFASTA.readline()
-    print(sequences)
+
+    for i in range(len(sequences)):
+        print(sequences[i])
