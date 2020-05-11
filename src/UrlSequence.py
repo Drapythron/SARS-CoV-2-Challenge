ecda620a110248dc0538ed3b3040a0c02cb18855
Martí Serratosa Sedó <64153663+martiserrase@users.noreply.github.com>
2020-05-11 21:45:39 +0200

    comentaris afegits

--- src/UrlSequence.py
+++ src/UrlSequence.py
@@ -9,16 +9,18 @@ class UrlSequence:
         self.__callFile()
 
     def __callFile(self):
+        # Enlace de descarga, finaliza con el accession a descargar
         url = ('http://www.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&rettype=fasta&id=' + self.__accession)
-        urllib.request.urlretrieve(url, 'sequencesFASTA.fasta')
+        urllib.request.urlretrieve(url, 'sequencesFASTA.fasta') # Obtenemos fichero con la sequencia ARN
 
-    def getSequence(self):
+    def getSequence(self):  
+        # Leemos el fichero fasta con la sequencia de ARN
         seqFASTA = open('sequencesFASTA.fasta')
         reader = seqFASTA.readline()
         reader = seqFASTA.readline()
         while len(reader) != 0:
-            reader = reader.replace('\n', '')
-            self.__sequence += reader
+            reader = reader.replace('\n', '')   
+            self.__sequence += reader           # Ajuntando todas sus lineas para facilitar el tratamiento de datos
             reader = seqFASTA.readline()
         return self.__sequence
 
